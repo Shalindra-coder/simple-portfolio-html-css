110884eeb6b7f72de40e9d6d5f7be26af0e89e1f
Shalindra-coder <156834865+Shalindra-coder@users.noreply.github.com>
2024-02-02 13:14:52 +0000

    Add files via upload

---
+++ oprator.c
@@ -0,0 +1,410 @@
+// #include <stdio.h>
+// #include <math.h>
+// int main()
+// {
+//    int a, b;
+
+   //  printf("hellow world\nEnter two numbers:\n");
+
+   //  scanf("%d%d",&a,&b);
+   //  printf("now add two numbers a and b\n");
+   //  int c = a+b;
+   //  printf("sum of two number:%d",c);
+
+   //  for(int i=a; i<=b;i++  ){
+   //      printf("\n%d",i);
+   // }
+
+   // question2:
+
+   //    printf("enter number a&b\n");
+   //    int a, b;
+   //    scanf("%d%d",&a,&b);
+
+   //    int sum=a+b;
+   //    printf("sum is a,b %d\n",sum);
+
+   // // question3;
+
+   // printf("enter number a&b&c");
+   // int a, b, c;
+   // scanf("%d%d%d",&a,&b,&c);
+
+   // int mul=a*b*c;
+   // printf("mul is a,b,c %d",mul);
+
+   // question4:
+
+   // printf("enter number a&b\n");
+   // int a,b;
+   // scanf("%d%d",&a,&b);
+
+   // int sum=a+b;
+   // int sub=a-b;
+   // int mul=a*b;
+   // printf("sum,sub,mul is a,b \n%d\n%d\n%d",sum,sub,mul);
+
+   // queston5:
+
+   // printf("enter nuber a&b\n");
+   // int a,b;
+   // scanf("%d%d",&a,&b);
+
+   // int average=(a+b)/2;
+   // printf("average,is a,b \n%d" ,average);
+
+   // question6:
+
+   // printf("enter number h&b\n");
+
+   // int h,b;
+   // scanf("%d%d",&h,&b);
+
+   // int area=0.5*(h*b);
+   // printf("area is h,b \n%d" ,area);
+
+   // question7:
+
+   //  printf("Enter numbers a and b:\n");
+
+   //  int a, b;
+   //  scanf("%d%d", &a, &b);
+
+   //  // Swapping using arithmetic operations
+   //  a = a + b;
+   //  b = a - b;
+   //  a = a - b;
+
+   //  printf("Swapped values: a = %d, b = %d\n", a, b);
+
+   // question8:
+
+   //  printf("enter number a,b,c:\n");
+
+   //  int a, b, c;
+   //  scanf("%d%d%d", &a,&b,&c);
+   //  int sum=(a*b)+c;
+   //  printf("sum is a,b,c %d\n",sum);
+
+   // qustion9:
+
+// printf("enter word a:");
+
+// char a=("there");
+// scanf("%c",&a);
+
+// printf("word is a",a);
+
+// question10:
+
+// printf("enter number a\n&b\n");
+// int a,b;
+// scanf("%d%d", &a,&b);
+// int sum=a+b;
+// int sub=a-b;
+// int mul=a*b;
+// int div=a/b;
+
+// printf("sum,sub,mul,div is a,b %d\n%d\n%d\n%d",sum,sub,mul,div);
+
+// question11:
+
+// printf("enter number a,b:");
+// int a,b;
+
+// scanf("%d%d", &a,&b);
+// int area=a*b;
+// int perimeter=2*(a+b);
+// printf("area,parimeter is a,b %d\n%d", area,perimeter);
+
+// question12:
+
+// printf("enter number a");
+// int a;
+// scanf("%d",&a);
+// int area=a*a;
+// int perimeter=4*a;
+
+// printf("area,perimeter is a %d\n%d", area,perimeter);
+
+// question13:
+
+// printf("enter number a,bandc");
+// int a,b,c;
+
+// scanf("%d%d%d",&a,&b,&c);
+
+// int semiperimeter=(a+b+c)/2;
+// printf("semiperimeter is  %d\n", semiperimeter);
+
+// question14:
+
+// printf("enter number is r");
+// int r;
+// scanf("%d",&r);
+
+// float area=22/7*(r*r);
+// printf("area is r %.2f\n",area);
+
+// question15:
+
+// printf("enter number c");
+// int c;
+
+// scanf("%d",&c);
+// float r=c/(2*3.14);
+// int D=2*r;
+// printf("D is c %f",r);
+
+// question16:
+
+// printf("enter number a");
+
+// int a;
+// scanf("%d",&a);
+// int lsa=4*(a*a);
+// int tsa=6*(a*a);
+
+// printf("lsa,tsa is a %d\n%d",lsa,tsa);
+
+// question17:
+
+// printf("enter number a,b");
+
+// int a,b;
+// scanf("%d,%d", &a,&b);
+
+// int sub=(a-b);
+// printf("sub is a,b %d",sub);
+
+// question18:
+
+// printf("enter number is a");
+
+// int a;
+// scanf("%d",&a);
+
+// int number=a%10;
+// printf("number is a %d",number);
+
+// question19:
+
+// question20:
+
+// printf("enter number a");
+// int a;
+// scanf("%d",&a);
+// int volume=a*a*a;
+
+// printf("volume is a %d",volume);
+
+// question21:
+
+// printf("enter number is a,bandc");
+
+// int a,b,c;
+
+// scanf("%d%d%d",&a,&b,&c);
+// int cv=a*b*c;
+// printf("cv is a,b,c %d\n%d\n%d", a,b,c);
+
+// question22:
+
+// printf("enter number is a,b");
+// int a,b;
+// scanf("%d%d",&a,&b);
+// int reminder=a%b;
+// printf("reminder is a,b %d\n%d");
+
+// question23:
+
+// printf("enter number a,b");
+
+// int a,b;
+// scanf("%d%d,", &a,&b);
+// int q=a/b;
+// printf("q is a,b %d",q);
+
+// question24:
+
+// int age=12;
+// printf("age is %d",age);
+
+// float age=23.56;
+// printf("age is %.2f",age);
+
+// char star='*';
+// printf("star is %c",star);
+
+// area of circle
+
+// int side;
+// printf("enter side ");
+// scanf("%d",&side);
+// printf("side is %d",side*side);
+
+// area of circle given redius;
+
+// float redu();
+// printf("enter redus");
+
+// scanf("%f", &redus);
+// printf("area is %f",3.14*redus*
+
+// int a = 5 * 2 - 2 * 3;
+// printf("%d\n", a);
+
+// int a=5*2/2*3;
+// printf("%d\n",a);
+
+// int a=5*(2/2)*3;
+// printf("%d\n",a);
+
+// int a=5+2/2*3;
+// printf("%d\n",a);
+
+// int x;
+// printf("enter a number");
+// scanf("%d",&x);
+// printf("%d", x % 2==0);
+
+// int issunday=1;
+// int issnow=0;
+// printf("%d", issunday || issnow);
+
+// int a;
+// printf("enter a number");
+// scanf("%d",&a);;
+// printf("%d", a>10 && a<100);
+
+// area of circle.
+
+// int redius;
+// printf("enter redius");
+// scanf("%d",&redius);
+// printf("area is circle %.2f\n",3.14*redius*redius)
+
+
+//******************************************************
+// question23:
+
+// int rate, totalamount;
+// printf("enter rate and totalamount:");
+// scanf("%d %d", &rate, &totalamount);
+// printf("bookcost is %d\n", totalamount/rate);
+
+//*****************************************************************************
+
+// question24:
+
+// int a;
+// printf("enter number:");
+// scanf("%d", &a);
+// printf("squar is a %d\n", a*a);
+//*****************************8
+// question25:
+//  int a;
+//  printf("enter number:");
+//  scanf("%d", &a);
+//  printf("qube is a %d\n", a*a*a);
+//**********************************************************************************
+//question26:
+   // all subject total marks:
+
+   // int hm, mm, em, sm, cm;
+   // printf("enter marks subject:");
+   // scanf("%d %d %d %d %d", &hm, &mm, &em, &sm, &cm);
+   // printf("total marks all subject %d\n", hm+mm+em+sm+cm);
+   //***********************************************************************************8
+
+   // question27:
+   // last digit find:
+
+   // int number;
+   // printf("enter number");
+   // scanf("%d", &number);
+   // printf("last digit %d\n", number%10);
+   //***************************************************************************************************
+   // question28:
+   // cuboid latral surface area total surface area:
+
+   // int height, widght, lenth;
+   // printf("enter height widght lenth");
+   // scanf("%d %d %d",&height, &widght, &lenth);
+   // printf("lsa area tsa %d\n%d\n", 2*height*(lenth*widght), 2*(lenth*widght+widght*height+lenth*height));
+   //*********************************************************************************************************
+   // question29:
+
+   // int  mrp,discount;
+   // printf("enter mrp discount:");
+   // scanf("%d%d", &mrp, &discount);
+   // int selingprice=mrp+(mrp*discount/100);
+   // printf("selingprice %d\n",selingprice);
+   //**************************************************************************************8
+   // question30:
+   // tempreture forenheight;
+
+   // int celsius;
+   // printf("enter celsius:");
+   // scanf("%d", &celsius);
+   // printf("forenhieght %d\n", (celsius*9)/32*5);
+   //************************************************************************
+
+   // question31:
+   // swapping:multiply,divide:
+   #include<stdio.h>
+   int main(){
+
+    int a, b;
+
+     
+    printf("Enter numbers a and b: ");
+    scanf("%d %d", &a, &b);
+
+     //Swap values of a and b without using a third variable
+    a = a * b;
+    b = a / b;
+    a = a / b;
+
+    // Output
+    printf("Swapped values: a = %d, b = %d\n", a, b);
+   }
+   //*************************************************************************************
+   // question32:
+   // swapping use third variable:
+
+   //  int a, b;
+
+   //  printf("Enter numbers a and b: ");
+   //  scanf("%d %d", &a, &b);
+
+   //  // Swapping using a third variable
+   //  int t = a;
+   //  a = b;
+   //  b = t;
+
+   //  printf("Swapped values: a = %d, b = %d\n", a, b);
+
+   //**************************************************************************************************
+
+   // question33:
+// #include<stdio.h>
+
+// int main() {
+//     int a = 10;
+//     int b = 20;
+//     int temp;
+
+    
+//      temp= a;
+//     a = b;
+//    b = temp;
+
+//     printf("%d %d\n", a, b);
+
+//     return 0;
+// }
+
+
+
+
